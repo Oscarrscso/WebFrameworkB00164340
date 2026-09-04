d44b04c4f2d0be13693919309173e0171aa16aba
Oscar Neiland <Oscarneiland@gmail.com>
2026-09-04 02:47:11 +0100

    /catalog/ shows products in text form, /catalog/product brings u to a. product page

--- BiaBuy/catalog/urls.py
+++ BiaBuy/catalog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("catalog/", views.index, name="index"),
+    path("", views.index, name="index"),
+    path("<str:item_name>/", views.product, name="product"),
 ]
--- BiaBuy/catalog/views.py
+++ BiaBuy/catalog/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from .models import Item
 
 def index(request):
-    return HttpResponse("Hello, world. You're in the Catalog.")
+    catalog = Item.objects.all()
+    output = ", ".join([Item.item_name for Item in catalog])
+    return HttpResponse(output)
+
+def product(request, item_name):
+    return HttpResponse("Product: %s\n Description: %s\n Price: %s" % item_name, item_desc)
